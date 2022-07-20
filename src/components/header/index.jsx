5bf797e41d14d3e519ed2d70b8527b723345e25b
yuanwenjun <2419917626@qq.com>
2022-07-20 23:53:14 +0800

    add: setState的用法

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,18 +1,43 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+import { menuList } from '../../config/menuConfig'
 import './index.less'
 
-export default class Header extends Component {
+class Header extends Component {
+
+    getTitle = () => {
+      let title
+      const path = this.props.location.pathname
+      menuList.forEach(item => {
+        if (item.key === path) {
+          title = item.title
+        } else if (item.children) {
+          const cItem = item.children.find(cItem => cItem.key === path)
+          if (cItem) {
+            title = cItem.title
+          }
+        }
+      })
+      return title
+    }
+
   render() {
+    const title = this.getTitle()
     return (
       <div className='header'>
         <div className="header-top">
             <span>你好，Admin</span>
         </div>
         <div className="header-bottom">
-            <div className='left'>首页</div>
-            <div className='right'>晴</div>
+            <div className='left'>{title}</div>
+            <div className='right'>
+                <span>晴</span>
+                <img src="http://api.map.baidu.com/images/weather/day/duoyun.png" alt='天气图片'/>
+            </div>
         </div>
       </div>
     )
   }
 }
+
+export default withRouter(Header)
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,31 @@
 import React, { Component } from 'react'
 
 export default class Home extends Component {
+  state = {
+    count: 1
+  }
+  test1 = () => {
+    // 函数写法 适用于新状态依赖于原状态
+    this.setState(state => ({
+      count: state.count + 1
+    }), ()=> {
+        // 在状态更新且界面更新之后回调
+        console.log('状态更新完毕，render之前调用')
+    })
+  }
+    // 对象写法 适用于新状态不依赖于原状态
+  test2 = () => {
+    const count = this.state.count + 1
+    this.setState({count})
+  }
   render() {
+    const count = this.state.count
     return (
-      <div>Home</div>
+      <div>
+        <p>Count的值为：{count}</p>
+        <button onClick={this.test1}>test1</button>
+        <button onClick={this.test2}>test2</button>
+      </div>
     )
   }
 }
